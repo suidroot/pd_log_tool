165b88ec12b710e594c8d145d36763f5e96b8b05
Ben Mason <locutus@the-collective.net>
2025-02-28 00:46:37 -0500

    loader update a csrf

--- csv_loader/loader.py
+++ csv_loader/loader.py
@@ -16,8 +16,8 @@ __email__ = "locutus@the-collective.net"
 __status__ = "Development"
 
 class config:
-    dispatch_url = 'http://127.0.0.1:8000/add/dispatch/'
-    arrest_url = 'http://127.0.0.1:8000/add/arrest/'
+    dispatch_url = 'http://pdlog.home.the-collective.net/add/dispatch/'
+    arrest_url = 'http://pdlog.home.the-collective.net/add/arrest/'
     debug = False
     error_file_suffix = "error.log"
     current_error_log = ""
--- webproject/log_site/production.py
+++ webproject/log_site/production.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(" ")
-
+CSRF_TRUSTED_ORIGINS = os.environ['CSRF_TRUSTED_ORIGINS'].split(" ")
 
 # Application definition
 
