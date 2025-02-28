f3c78a3535b16f8d9a0c73802be5f9495eb6171e
Ben Mason <locutus@the-collective.net>
2025-02-28 00:14:59 -0500

    allowed hosts

--- webproject/log_site/production.py
+++ webproject/log_site/production.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
-ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS']
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(" ")
 
 
 # Application definition
